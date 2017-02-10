dd9313e079bd14906c9bb54492319bec66df538b
starttirth1998 <tirthmaniar1998@gmail.com>
2017-02-10 20:04:10 +0530

    Start

---
+++ header.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+using namespace std;
